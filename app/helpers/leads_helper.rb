bbfcef8e58348652a69bc6febe696fbcc02a66d1
Matt Perejda <matt.perejda@gmail.com>
2015-03-02 00:16:39 -0500

    split table and leads helpers

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,2 +1,15 @@
 module LeadsHelper
+
+  def hot_leads
+    Lead.all.select { |lead| lead.temperature == "hot"}
+  end
+
+  def medium_leads
+    Lead.all.select { |lead| lead.temperature == "med"}
+  end
+
+  def cold_leads
+    Lead.all.select { |lead| lead.temperature == "cold"}
+  end
+
 end
